9acd36cc586b654ad83f6273b21ee67454420c6f
xabb0s <abbosxudoyberganov2005@gmail.com>
2022-07-17 20:13:28 +0500

    fixed bugs

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const btn = document.getElementById("btn")
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
 const msgBtn = document.getElementById("msg-btn")
 
 btn.onclick = () => {
+  window.location.href = "./igIndex.html"
+}
+
+btn2.onclick = () => {
+  window.location.href = "./igIndex.html"
+}
+
+btn3.onclick = () => {
   window.location.href = "./igIndex.html"
 }
 
